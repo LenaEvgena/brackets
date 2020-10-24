65618898d17ce4fe0ff1485548d40bb97f7a3ce6
Alena Astravukh <helen151204@gmail.com>
2020-10-24 22:02:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+    let stack = [];
+    let strArray = str.split('');
+
+    for (let j = 0; j < strArray.length; j++) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            let item = bracketsConfig[i];
+
+            if (stack.length && strArray[j] === item[1] && stack[stack.length - 1] === item[0]) {
+                stack.pop();
+                break;
+            }
+            if (strArray[j] === item[0]) {
+                stack.push(strArray[j]);
+                break;
+            }
+            if (stack.length === 0 && strArray[j] === item[1]) {
+                return false;
+            }
+        }
+    }
+
+    if (stack.length === 0) {
+        return true;
+    } else {
+        return false;
+    };
+};
